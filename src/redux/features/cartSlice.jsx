1840ded154a238b5bd816a6f2ec7d345b42703c5
arjunmaurya96 <arjunmaurya9621192312@gmail.com>
2024-02-04 14:15:24 +0530

    restorent

---
+++ src/redux/features/cartSlice.jsx
@@ -0,0 +1,57 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+const initialState = {
+    carts: []
+}
+
+//card slice 
+
+const cartSlice = createSlice({
+    name: "cartslice",
+    initialState,
+    reducers: {
+        // add to cart 
+        addToCart: (state, action) => {
+            // console.log("action",action);
+
+            const IteamIndex = state.carts.findIndex((iteam) => iteam.id === action.payload.id);
+            console.log(IteamIndex);
+
+            if (IteamIndex >= 0) {
+                state.carts[IteamIndex].qnty += 1
+            } else {
+
+                const temp = { ...action.payload, quty: 1 }
+                state.carts = [...state.carts, temp]
+            }
+        },
+
+        //remove partucular iteams 
+
+        removeToCart:(state,action)=>{
+            const data = state.carts.filter((ele)=>ele.id !== action.payload)
+            state.carts = data;
+        },
+
+        // remove single iteams 
+        removeSingleIteams:(state,action)=>{
+            const IteamIndex_dec = state.carts.findIndex((iteam) => iteam.id === action.payload.id);
+
+            if(state.carts[IteamIndex_dec].qnty >=1){
+                state.carts[IteamIndex_dec].qnty -= 1
+            }
+
+
+        },
+         // clear cart 
+         emptycartIteam:(state,action)=>{
+            state.carts = []
+
+         }
+
+    }
+});
+
+export const { addToCart, removeToCart, removeSingleIteams, emptycartIteam} = cartSlice.actions;
+
+export default cartSlice.reducer;
